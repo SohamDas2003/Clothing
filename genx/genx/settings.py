072e11fa166d2816ea40a37a8a85cfb190fa4902
TheLegend-2003 <sohamdas704@gmail.com>
2023-10-11 11:15:44 +0530

    commit:2

--- genx/genx/settings.py
+++ genx/genx/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'clothing.apps.ClothingConfig'
+    'clothing.apps.ClothingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
