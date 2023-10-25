23ca2290330c695d5c6cb9d0a84aaeecf2c2ca8d
TheLegend-2003 <sohamdas704@gmail.com>
2023-10-25 12:25:16 +0530

    commit:6

--- genx/clothing/urls.py
+++ genx/clothing/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from clothing import views
 
+app_name= 'clothing'
+
 urlpatterns = [
     path('home/', views.index, name='index'),
     path('detail/<int:item_id>/', views.detail, name='detail'),
