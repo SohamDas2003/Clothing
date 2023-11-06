157f19a3b0f9e6cf6b4f66679c32a8219fd12a0b
TheLegend-2003 <sohamdas704@gmail.com>
2023-11-06 17:52:56 +0530

    commit:9

--- genx/users/views.py
+++ genx/users/views.py
@@ -3,27 +3,34 @@ from django.contrib.auth.forms import UserCreationForm
 from users.forms import RegisterForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
 
         if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request,
+                'Welcome {}, your account has been successfully created. Now you may log in'.format(
+                    username)
+            )
             form.save()
-            return redirect('clothing:index')
+            return redirect('login')
 
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
         context = {
-            'form':form
+            'form': form
         }
 
         return render(request, 'users/register.html', context)
-    
+
 
 def login_view(request):
 
@@ -32,17 +39,43 @@ def login_view(request):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
 
-        if user is not None:
+        if user is None:
+            messages.success(
+                request,
+                'Invalid Login, try again'
+            )
+            return redirect('login')
+
+        elif user.is_superuser:
             login(request, user)
+            messages.success(
+                request,
+                'Welcome Superuser {}, you have been successfully logged in'.format(
+                    request.user.username)
+            )
+            return redirect('clothing:index')
+
+        elif user is not None:
+            login(request, user)
+            messages.success(
+                request,
+                'Welcome {}, you have been successfully logged in'.format(
+                    request.user.username)
+            )
             return redirect('clothing:index')
 
     return render(request, 'users/login.html')
 
 
 def logout_view(request):
+    messages.success(
+        request,
+        '{}, you have successfully logged out'.format(request.user.username)
+    )
+    logout(request)
+    return redirect('clothing:index')
 
-    if request.method == 'POST':
-        logout(request)
-        return redirect('clothing:index')
 
-    return render(request, 'users/logout.html')
+@login_required
+def profilepage(request):
+    return render(request, 'users/profile.html')
