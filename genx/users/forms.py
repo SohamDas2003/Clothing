ca72c07ad883432907c21274ed08038641dd2255
TheLegend-2003 <sohamdas704@gmail.com>
2024-01-15 21:45:55 +0530

    commit:29

--- genx/users/forms.py
+++ genx/users/forms.py
@@ -1,18 +1,36 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import RegexValidator
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    mob_num = forms.IntegerField(
+    first_name = forms.CharField(
+        validators=[
+            RegexValidator(
+                regex='^[a-zA-Z]+$',
+                message='First Name must contain only alphabets.',
+            ),
+        ],
+    )
+    last_name = forms.CharField(
+        validators=[
+            RegexValidator(
+                regex='^[a-zA-Z]+$',
+                message='Last Name must contain only alphabets.',
+            ),
+        ],
+    )
+    mob_num = forms.CharField(
         label='Mobile Number',
         validators=[
-            MaxValueValidator(9999999999, message='Mobile number must be at most 10 digits.'),
-            MinValueValidator(0, message='Enter a valid mobile number with only numeric characters.'),
-        ],)
+            RegexValidator(
+                regex='^[0-9]{10}$',
+                message='Mobile Number must be a 10-digit numeric value.',
+            ),
+        ],
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'mob_num', 'password1', 'password2']
